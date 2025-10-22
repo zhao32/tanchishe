17aefd01cb7d75ad61c264d1e45c13a7f224f58f
ZHAO <zhao_zq32@163.com>
2025-10-22 18:03:08 +0800

    修改

--- assets/scripts/CameraFollow.ts
+++ assets/scripts/CameraFollow.ts
@@ -70,21 +70,45 @@ export default class CameraFollow extends cc.Component {
 
         // 计算目标位置
         const targetPosition = this.target.position; //.add(cc.v3(this.offsetX, 0));
-        if (targetPosition.x + this.ndCanvas.width / 2 > this.ndBG.height / 2) {
-            targetPosition.x = this.ndBG.height / 2 - this.ndCanvas.width / 2;
+        if (targetPosition.x + this.ndCanvas.width / 2 > this.ndBG.width / 2) {
+            targetPosition.x = this.ndBG.width / 2 - this.ndCanvas.width / 2;
         }
-        if (targetPosition.x - this.ndCanvas.width / 2 < -this.ndBG.height / 2) {
-            targetPosition.x = -(this.ndBG.height / 2 - this.ndCanvas.width / 2);
+        if (targetPosition.x - this.ndCanvas.width / 2 < -this.ndBG.width / 2) {
+            targetPosition.x = -(this.ndBG.width / 2 - this.ndCanvas.width / 2);
         }
-        if (targetPosition.y + this.ndCanvas.height / 2 > this.ndBG.width / 2) {
-            targetPosition.y = this.ndBG.width / 2 - this.ndCanvas.height / 2;
+        if (targetPosition.y + this.ndCanvas.height / 2 > this.ndBG.height / 2) {
+            targetPosition.y = this.ndBG.height / 2 - this.ndCanvas.height / 2;
         }
-        if (targetPosition.y - this.ndCanvas.height / 2 < -this.ndBG.width / 2) {
-            targetPosition.y = -(this.ndBG.width / 2 - this.ndCanvas.height / 2);
+        if (targetPosition.y - this.ndCanvas.height / 2 < -this.ndBG.height / 2) {
+            targetPosition.y = -(this.ndBG.height / 2 - this.ndCanvas.height / 2);
         }
         // 平滑移动摄像机
         // let newPosition = this.node.position.lerp(targetPosition, this.smoothSpeed);
         // newPosition.y = 0;
         this.node.position = targetPosition;
     }
+
+    // protected lateUpdate(dt: number): void {
+    //     if (!this.target) return;
+    //     if (!this.isMove) return;
+
+    //     // 计算目标位置
+    //     const targetPosition = this.target.position; //.add(cc.v3(this.offsetX, 0));
+    //     if (targetPosition.x + this.ndCanvas.width / 2 > this.ndBG.height / 2) {
+    //         targetPosition.x = this.ndBG.height / 2 - this.ndCanvas.width / 2;
+    //     }
+    //     if (targetPosition.x - this.ndCanvas.width / 2 < -this.ndBG.height / 2) {
+    //         targetPosition.x = -(this.ndBG.height / 2 - this.ndCanvas.width / 2);
+    //     }
+    //     if (targetPosition.y + this.ndCanvas.height / 2 > this.ndBG.width / 2) {
+    //         targetPosition.y = this.ndBG.width / 2 - this.ndCanvas.height / 2;
+    //     }
+    //     if (targetPosition.y - this.ndCanvas.height / 2 < -this.ndBG.width / 2) {
+    //         targetPosition.y = -(this.ndBG.width / 2 - this.ndCanvas.height / 2);
+    //     }
+    //     // 平滑移动摄像机
+    //     // let newPosition = this.node.position.lerp(targetPosition, this.smoothSpeed);
+    //     // newPosition.y = 0;
+    //     this.node.position = targetPosition;
+    // }
 }
